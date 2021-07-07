c668591adeaae05d3dad456d143c57e66b999d26
matheus20021998 <56241825+matheus20021998@users.noreply.github.com>
2021-07-07 00:07:26 -0300

    Features

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { Container } from './styles';
+import RadioGroup,{Radio} from "react-native-radio-input";
 
 export default () => {
+    getChecked = (value) =>{
+        console.log(value)
+    }
+
+    
     return (
         <Container>
-            <Text>Search</Text>
+            <RadioGroup getChecked={this.getChecked}>
+                <Radio iconName={"lens"} label={"The first option"} value={"A"}/>
+                <Radio iconName={"lens"} label={"The first option"} value={"B"}/>
+                <Radio iconName={"lens"} label={"I need numbers"} value={1}/>
+                <Radio label={"Is IconName Optional?"} value={"Yes"}/>
+                <Radio label={"Show Sentence Value"} value={"This is a Sentence"}/>
+            </RadioGroup>
         </Container>
     );
 }
